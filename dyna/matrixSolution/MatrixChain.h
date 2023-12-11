e95a7ed5046d09075d2be92e9f082f0ee003c67c
RichardBrowning <46521212+RichardBrowning@users.noreply.github.com>
2023-12-12 03:29:19 +0800

    ijk

--- dyna/matrixSolution/MatrixChain.h
+++ dyna/matrixSolution/MatrixChain.h
@@ -1,13 +1,20 @@
 #pragma once
 #include <vector>
+#include <string>
 #include "Matrix.h"
 class MatrixChain {
     private:
         std::vector<Matrix*> *matricesPtr = nullptr;
-        int matrixMultiplicationCount[10][10] = {};
+        int matrixMultiplicationCount[10][10] = {0};
+        int kValue[10][10] = {0};
+        int P[11] = {};
     public:
         MatrixChain(std::vector<Matrix*> *matrices){
             this->matricesPtr = matrices;
+            P[0] = this->matricesPtr->at(0)->rows;
+            for (int i = 0; i < this->matricesPtr->size(); i++){
+                P[i+1] = this->matricesPtr->at(i)->cols;
+            }
         }
 
         ~MatrixChain(){
@@ -15,24 +22,21 @@ class MatrixChain {
         }
 
         int solve(){
-            // get P's
-            int P[11] = {};
-            P[0] = this->matricesPtr->at(0)->rows;
-            for (int i = 0; i < this->matricesPtr->size(); i++){
-                P[i+1] = this->matricesPtr->at(i)->cols;
-            }
             //debug output
             // for (int i = 0; i <= this->matricesPtr->size(); i++){
             //     std::cout << P[i] << std::endl;
             // }
 
-            for (int k = 1; k < this->matricesPtr->size(); k++){
-                for (int i = 0; i < this->matricesPtr->size(); i++){
-                    if (i + k >= this->matricesPtr->size()) break;
-                    matrixMultiplicationCount[i][i+k] = matrixMultiplicationCount[i][i] + matrixMultiplicationCount[i+1][i+k] + P[i] * P[i+1] * P[i+k+1];
-                    for (int j = i + 1; j < i + k; j++){
-                        int temp = matrixMultiplicationCount[i][j] + matrixMultiplicationCount[j+1][i+k] + P[i] * P[j+1] * P[i+k+1];
-                        if (temp < matrixMultiplicationCount[i][i+k]) matrixMultiplicationCount[i][i+k] = temp;
+            for (int i = 1; i < this->matricesPtr->size(); i++){
+                for (int j = 0; j < this->matricesPtr->size(); j++){
+                    if (i + j >= this->matricesPtr->size()) break;
+                    matrixMultiplicationCount[j][i+j] = matrixMultiplicationCount[j][j] + matrixMultiplicationCount[j+1][i+j] + P[j] * P[j+1] * P[i+j+1];
+                    for (int k = j + 1; k < i + j; k++){
+                        int temp = matrixMultiplicationCount[j][k] + matrixMultiplicationCount[k+1][i+j] + P[j] * P[k+1] * P[i+j+1];
+                        if (temp < matrixMultiplicationCount[j][i+j]) {
+                            matrixMultiplicationCount[j][i+j] = temp;
+                            kValue[j][i+j] = k;
+                        }
                     }
                 }
             }
@@ -47,5 +51,25 @@ class MatrixChain {
                 std::cout << std::endl;
             }
             std::cout << "The minimal number of multiplications is " << matrixMultiplicationCount[0][this->matricesPtr->size()-1] << std::endl;
+            for (int i = 0; i < this->matricesPtr->size(); i++){
+                for (int j = 0; j < this->matricesPtr->size(); j++){
+                    std::cout << kValue[i][j] << "\t";
+                }
+                std::cout << std::endl;
+            }
+            std::cout << "The optimal parenthesization is " << this->getParenthesizedForm(0, this->matricesPtr->size()-1) << std::endl;
+        }
+
+        std::string getParenthesizedForm(int i, int j){
+            std::string result = "";
+            result += "(";
+            if (i == j){
+                result += "0";
+            } else {
+                result += getParenthesizedForm(i, kValue[i][j]);
+                result += getParenthesizedForm(kValue[i][j]+1, j);
+                result += std::to_string(P[i]) + "x" + std::to_string(P[kValue[i][j]+1]) + "x" + std::to_string(P[j+1]);
+            }
+            return result;
         }
 };
